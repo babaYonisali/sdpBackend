b8759aaa4eeda229922722d384519988da635344
babaYonisali <2460755@students.wits.ac.za>
2024-08-12 14:50:30 +0200

    cors fixing

--- app.js
+++ app.js
@@ -9,14 +9,8 @@ const allowedOrigins = [
     'http://localhost:3000', // Add your local development URL here
     'http://localhost:3001'  // If your local frontend runs on a different port, add it here
   ];
-  const corsOptions = {
-    origin: (origin, callback) => {
-        if (allowedOrigins.includes(origin) || !origin) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
+  const corsOptions = { 
+    origin:'*',
     optionsSuccessStatus: 200
   };
   app.use(cors(corsOptions));
